4e00966e52b96bf0f79364a6859c1c5301185c22
Bentley James Oakes <bentleyjoakes@gmail.com>
2014-03-09 19:32:32 -0400

    Add Optimization class and wire in constant folding

---
+++ CBDSimulator/Optimizations/Optimization.py~
@@ -0,0 +1,17 @@
+
+
+class Optimization:
+
+    def __init__(self, simulator):
+        self.simulator = simulator
+        
+        
+    def optimize(self, model):
+        pass
+        
+    def analyze(self, model):
+        pass
+        
+    def transform(self, model):
+        pass
+        
---
+++ CBDSimulator/optimizationExperiment.py
@@ -0,0 +1,39 @@
+from CBDsimulator import *
+import numpy as np
+from Models.ExampleCBD import *
+from Optimizations.ConstantFoldingOptimization import *
+
+class OptimizationExperiment:
+
+  def __init__(self):
+    self.model  = ExampleCBD("ExampleCBD")
+    self.simulator = CBDsimulator(self.model)
+ 
+  def run(self, num_steps=10): # one step = {NOW}
+    for i in range(num_steps):
+      self.simulator.step()
+
+  def dumpModel(self):
+    self.model.dump()
+
+  def dumpSignals(self):
+    self.model.dumpSignals()
+
+  def showScopes(self,t):
+      self.model.showScopes(t)
+  
+if __name__=="__main__":
+
+  experiment = OptimizationExperiment()
+  #experiment.dumpModel()
+  
+  CBDOpt = ConstantFoldingOptimization(experiment.simulator)
+  CBDOpt.optimize(experiment.model)
+  
+  #experiment.dumpModel()
+  #experiment.run()
+  #experiment.dumpSignals()
+  
+  #t = np.arange(0.0, 10.0, 0.001)
+  #experiment.showScopes(t)
+
